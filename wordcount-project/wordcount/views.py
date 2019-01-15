ab492e7f01ae6016c89d968f01d0d0456ee162d0
vlsvictor <victor.lopezselva@appdirect.com>
2019-01-15 09:26:34 -0500

    fixing wordcount

---
+++ wordcount-project/wordcount/views.py
@@ -0,0 +1,24 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse('Hello')
+    return render(request, 'home.html')
+
+def count(request):
+
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    word_dict = {}
+    for word in wordlist:
+        if word in word_dict:
+            word_dict[word] += 1
+        else:
+            word_dict[word] = 1
+
+    
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist), 'dict':word_dict.items()})
